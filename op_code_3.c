f6e31f6737f5e35866d5ba415ec767926143a972
anversa-pro <angelavergaras@gmail.com>
2021-09-22 10:16:15 -0500

    [MODIFY] add descriptions to functions

--- op_code_3.c
+++ op_code_3.c
@@ -1,11 +1,23 @@
 #include "monty.h"
 
+/**
+ * op_pchar - function to print the character on top of the list
+ * @stack: double pointer to a double linked list
+ * @line_number: counter of FILE lines.
+ * Return: None
+ */
 void op_pchar(stack_t **stack, unsigned int line_number)
 {
 	line_number = 0;
 	printf("%c\n", (*stack)->n);
 }
 
+/**
+ * op_pstr - function to print a string
+ * @stack: double pointer to a double linked list
+ * @line_number: counter of FILE lines.
+ * Return: None
+ */
 void op_pstr(stack_t **stack, unsigned int line_number)
 {
 	line_number = 0;
