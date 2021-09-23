00f71c2364f8e4cffb3920139998177bc91c8ccd
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-09-22 19:43:25 -0500

    [FIX] - Fix code in edge case only spaces in line

--- main.c
+++ main.c
@@ -19,6 +19,30 @@ void change_last_character(char *line)
 }
 
 /**
+ * validate_line - validate if a line is diferent to ' ' or null
+ * @line: Line to modify
+ * Return: 1 to next or 0 to entry
+ */
+unsigned int validate_line(char *line)
+{
+	int i = 0;
+
+	if (!line[0])
+	{
+		return (1);
+	}
+	while (line[i])
+	{
+		if (line[i] != ' ')
+		{
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+
+/**
  * main - Entry point of project
  * @argc: acount of arguments
  * @argv: value of any argument
@@ -28,9 +52,8 @@ int main(int argc, char *argv[])
 {
 	FILE *fp = NULL;
 	char *c = NULL;
-	unsigned int line = 1;
-	size_t sizeC = 0;
-	stack_t *stackMonty = NULL;
+	unsigned int line = 1, valLine = 0;
+	size_t sizeC = 0, stack_t *stackMonty = NULL;
 	void (*cp_get_op_code)(stack_t **stack, unsigned int line);
 
 	if (argc != 2)
@@ -46,9 +69,10 @@ int main(int argc, char *argv[])
 	}
 	while (getline(&c, &sizeC, fp) != -1)
 	{
-		change_last_character(c);
-		dataStruct.lineTokenized = c;
-		if (dataStruct.lineTokenized[0])
+		valLine = 0;
+		change_last_character(c), dataStruct.lineTokenized = c;
+		valLine = validate_line(dataStruct.lineTokenized);
+		if (!valLine)
 		{
 			cp_get_op_code = (*get_op_code)();
 			if (cp_get_op_code)
